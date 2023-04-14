d79be596ae7ac3a6518d3cb26ab22a89597b16f4
trewos <n.samokhvalov@edu.internetdevels.com>
2023-04-14 17:13:00 +0300

    1.0.2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const navbarToggle = document.querySelector(".navbar-toggle");
-const navbarMenu = document.querySelector(".navbar-menu");
-
-navbarToggle.addEventListener("click", () => {
-  navbarMenu.classList.toggle("active");
-});
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+navbarToggle.addEventListener('click', function(){
+  navbarToggle.classList.toggle('active');
+  navbarMenu.classList.toggle('active');
+})
