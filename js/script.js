1709ca3f60cf2db01169906057a8de04e15c77cb
trewos <n.samokhvalov@edu.internetdevels.com>
2023-04-24 11:45:32 +0300

    1.1.0

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 /* Натискання на burger-menu */
+
 const menuClick = document.querySelector('.menu__container');
 const menuList = document.querySelector('.menu__list');
 const menuToggle = document.querySelector('.menu__toggle');
-menuClick.addEventListener('click', function(){
-  menuToggle.classList.toggle('active');
-  menuList.classList.toggle('active');
+menuClick.addEventListener('click', function() {
+  menuToggle.classList.toggle('active'); /* Додає або видаляє 'active' до / з елемента 'menuToggle' */
+  menuList.classList.toggle('active'); /* Додає або видаляє 'active' до / з елемента 'menuList' */
 });
+
 /* Налаштування slider */
+
 $(document).ready(function() {
   $('.slider__container').slick({
     infinite: true,
@@ -18,25 +21,32 @@ $(document).ready(function() {
     autoplaySpeed: 4000,
   });
 });
+
 /* Виконує функцію, коли при наведенні на елемент, з'являються підкатегорії. */
+
 $(document).ready(function() {
-  $('.content__category__auto, .content__category__happliances, .content__category__mobile, .content__category__pc, .content__category__clothes, .content__category__house').hover(function() {
+  $('.content__category-auto, .content__category-happliances, .content__category-mobile, .content__category-pc, .content__category-clothes, .content__category-house').hover(function() {
     $(this).find('ul').addClass('active');
   }, function() {
     $(this).find('ul').removeClass('active');
   });
 });
 
-const link = document.querySelector('#header__contacts__feedback')
+/* При натисканні на посилання '#header__contacts-feedback' відкриває вікно 'feedback' з fade ефектом */
+
+const link = document.querySelector('#header__contacts-feedback')
 const feedback = document.querySelector('#feedback')
 link.addEventListener('click', function(){
   event.preventDefault();
   feedback.classList.add('show' , 'fade-in');
   setTimeout(() => {
     feedback.classList.remove('fade-in');
-  }, 290);
+  }, 280);
 });
-const hideLinks = document.querySelectorAll('.feedback__container__close');
+
+/* При натисканні на посилання .feedback__close-btn' закриває вікно 'feedback' з fade ефектом */
+
+const hideLinks = document.querySelectorAll('.feedback__close-btn');
 hideLinks.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault();
@@ -45,6 +55,6 @@ hideLinks.forEach(link => {
     setTimeout(() => {
       feedback.classList.remove('show');
       feedback.classList.remove('fade-out');
-    }, 290);
+    }, 280);
   });
 });
